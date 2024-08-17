634876851f900a00667f325e686b77d373623b5a
Kiana8181 <kianahashemi8@gmail.com>
2024-08-17 16:30:35 +0330

    Fix bug:updating issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -39,7 +39,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      if (issue) await axios.patch("/api/issues" + issue.id, data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
